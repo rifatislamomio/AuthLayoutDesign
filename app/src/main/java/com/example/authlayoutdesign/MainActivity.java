02ea12a6cf05fd3643b28a5399681b2689c1a238
omio89 <rifat.islam173@northsouth.edu>
2020-04-19 14:09:39 +0600

    initial

---
+++ app/src/main/java/com/example/authlayoutdesign/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.authlayoutdesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+         btn = findViewById(R.id.click);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               startActivity(new Intent(getApplicationContext(),VerificationPage.class));
+                //setContentView(R.layout.activity_verification_page);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/authlayoutdesign/VerificationPage.java
@@ -0,0 +1,191 @@
+package com.example.authlayoutdesign;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
+
+public class VerificationPage extends AppCompatActivity {
+
+    Toolbar toolbar;
+    Button homeButton,confirmButton,editNumberButton;
+    TextView textViewResendOTP;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verification_page);
+
+
+        homeButton = findViewById(R.id.home_button_verification_page);
+        toolbar = findViewById(R.id.verification_page_toolbar);
+        setSupportActionBar(toolbar);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
+
+        ((EditText)findViewById(R.id.editTextDigit1)).setCursorVisible(false);
+        findViewById(R.id.editTextDigit1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((EditText) findViewById(R.id.editTextDigit1)).setCursorVisible(true); }});
+        ((EditText)findViewById(R.id.editTextDigit1)).addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(((EditText)findViewById(R.id.editTextDigit1)).getText().toString().length()==1)
+                {
+                    findViewById(R.id.editTextDigit2).clearFocus();
+                    findViewById(R.id.editTextDigit2).requestFocus();
+                    ((EditText) findViewById(R.id.editTextDigit2)).setCursorVisible(true);
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable s) { }
+        });
+        ((EditText)findViewById(R.id.editTextDigit2)).addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(((EditText)findViewById(R.id.editTextDigit2)).getText().toString().length()==1)
+                {
+                    findViewById(R.id.editTextDigit3).clearFocus();
+                    findViewById(R.id.editTextDigit3).requestFocus();
+                    ((EditText) findViewById(R.id.editTextDigit3)).setCursorVisible(true);
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable s) { }
+        });
+        ((EditText)findViewById(R.id.editTextDigit3)).addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(((EditText)findViewById(R.id.editTextDigit3)).getText().toString().length()==1)
+                {
+                    findViewById(R.id.editTextDigit4).clearFocus();
+                    findViewById(R.id.editTextDigit4).requestFocus();
+                    ((EditText) findViewById(R.id.editTextDigit4)).setCursorVisible(true);
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable s) { }
+        });
+        ((EditText)findViewById(R.id.editTextDigit4)).addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(((EditText)findViewById(R.id.editTextDigit4)).getText().toString().length()==1)
+                {
+                    findViewById(R.id.editTextDigit5).clearFocus();
+                    findViewById(R.id.editTextDigit5).requestFocus();
+                    ((EditText) findViewById(R.id.editTextDigit5)).setCursorVisible(true);
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable s) { }
+        });
+        ((EditText)findViewById(R.id.editTextDigit5)).addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(((EditText)findViewById(R.id.editTextDigit5)).getText().toString().length()==1)
+                {
+                    findViewById(R.id.editTextDigit6).clearFocus();
+                    findViewById(R.id.editTextDigit6).requestFocus();
+                    ((EditText) findViewById(R.id.editTextDigit6)).setCursorVisible(true);
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable s) { }
+        });
+        ((EditText)findViewById(R.id.editTextDigit6)).addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(((EditText)findViewById(R.id.editTextDigit6)).getText().toString().length()==1)
+                {
+                    ((EditText) findViewById(R.id.editTextDigit6)).setCursorVisible(false);
+                    findViewById(R.id.btn_confirm).clearFocus();
+                    findViewById(R.id.btn_confirm).requestFocus();
+                    hideSoftInput();
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable s) { }
+        });
+
+
+
+        textViewResendOTP = findViewById(R.id.TextViewResendOTP);
+        textViewResendOTP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getApplicationContext(),"RESEND OTP",Toast.LENGTH_SHORT).show();
+                textViewResendOTP.setEnabled(false);
+                textViewResendOTP.setTextColor(getResources().getColor(R.color.colorInactive));
+                ToggleResendTextView(textViewResendOTP);
+                //Write ResendOTP Function Here
+
+            }
+        });
+
+        ToggleResendTextView(textViewResendOTP);
+        confirmButton = findViewById(R.id.btn_confirm);
+        editNumberButton = findViewById(R.id.btn_change_number);
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Write OTP Submission Function Here
+            }
+        });
+
+        editNumberButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //I will do it :)
+            }
+        });
+
+
+    }
+
+    //Methods
+    public void hideSoftInput() {
+        View view1 = this.getCurrentFocus();
+        if(view1!= null){
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view1.getWindowToken(), 0);
+        }
+    }
+    public void ToggleResendTextView(final TextView textView)
+    {
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                textView.setEnabled(true);
+                textView.setTextColor(getResources().getColor(R.color.colorActive));
+            }
+        }, 30000);
+    }
+}
